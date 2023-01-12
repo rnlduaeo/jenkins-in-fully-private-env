706e31051dc7e7b0b021db63c86a5f38068bbf1d
haemikim <haemikim@amazon.com>
2023-01-12 16:49:33 +0900

    Refactor terraform with tfautomv

--- .history/packer/instances/modules/jenkins_agent/output_20230111161858.tf
+++
@@ -1,4 +0,0 @@
-output "spot-request-id" {
-  description = "spot request id"
-  value       = aws_spot_fleet_request.jenkins-agent-spot-request.id
-}
--- .history/packer/instances/modules/jenkins_agent/output_20230112131245.tf
+++
@@ -1,4 +0,0 @@
-output "spot_request_id" {
-  description = "spot request id"
-  value       = aws_spot_fleet_request.jenkins_agent.id
-}
